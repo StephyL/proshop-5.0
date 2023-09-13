dc69c347b0cc04f3fc1ed2499a159169b37d8993
StephyL <91197564+StephyL@users.noreply.github.com>
2023-09-13 16:40:58 +1000

    server app to api/products directly

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,9 @@ app.use(express.urlencoded({ extended: true }));
 //Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
